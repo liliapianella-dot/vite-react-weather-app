d59c15ed38d85a2f429370c71228d9720d1613e1
pianellalilia-dot <pianella.lilia@gmail.com>
2026-01-16 06:54:39 +0100

    Added the GitHub Repository URL

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project is created by{" "}
+        <a href="https://www.liliapianella.com/" target="_blank">
+          Lilia
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/liliapianella-dot/vite-react-weather-app"
+          target="_blank"
+        >
+          open source on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
