e23f2cc6687ef5a0eadb93a00ab7e12c59ceafe5
pianellalilia-dot <pianella.lilia@gmail.com>
2026-01-16 07:23:28 +0100

    Added Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{" "}
-        <a href="https://www.liliapianella.com/" target="_blank">
-          Lilia
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/liliapianella-dot/vite-react-weather-app"
-          target="_blank"
-        >
-          open source on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <a className="btn btn-primary">button</a>
+        <footer>
+          This project is created by{" "}
+          <a href="https://www.liliapianella.com/" target="_blank">
+            Lilia
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/liliapianella-dot/vite-react-weather-app"
+            target="_blank"
+          >
+            open source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
